c82cc0f25c64f5e942dbc7ee045b9b5da6d08723
wmc24 <81317269+wmc24@users.noreply.github.com>
2021-03-24 16:59:08 +0000

    Update potential_field_plotting.py
    Trimmed down and modified from assignment 2 to our planet version

--- potential_field_plotting.py
+++ potential_field_plotting.py
@@ -4,21 +4,30 @@ from mpl_toolkits import mplot3d
 
 
 grid_spacing = 50
-colourmap = 'viridis'
 colourmap = 'jet'
 edgecol = 'k'
 edgew = 0.5
+
+# Default Arena
+DIMENSIONS = 2
+MAX_SPEED = .5
+SIZE_OF_UNIVERSE = 10.
+PLANET_POSITION = np.array([3., 2., .5][:DIMENSIONS], dtype=np.float32)
+PLANET_RADIUS = .3
+STATIONARY_SPACESHIP = np.concatenate((np.array([-.3, 4.], dtype=np.float32),  normalize(np.array([.3, -4.], dtype=np.float32))))
+METEOROID = np.concatenate((np.array([-.3, 4.], dtype=np.float32),  MAX_SPEED * 2 * normalize(np.array([.3, -4.], dtype=np.float32))))
+GOAL_POSITION = np.array([5., 5.], dtype=np.float32)
+START_POSITION = np.array([-5., -5.], dtype=np.float32)
 
 
 """ Plotting the Goal Potential """
 
 
 def goal_potential(x, y):
-    goal = (1.5, 1.5)
-    return 0.5 * (x - goal[0])**2 + 0.5 * (y - goal[1])**2
+    return 0.5 * (x - GOAL_POSITION[0])**2 + 0.5 * (y - GOAL_POSITION[1])**2
 
-x = np.linspace(-2, 2, grid_spacing)
-y = np.linspace(-2, 2, grid_spacing)
+x = np.linspace(-SIZE_OF_UNIVERSE/2, SIZE_OF_UNIVERSE/2, grid_spacing)
+y = np.linspace(-SIZE_OF_UNIVERSE/2, SIZE_OF_UNIVERSE/2, grid_spacing)
 
 X, Y = np.meshgrid(x, y)
 Z = goal_potential(X, Y)
@@ -31,117 +40,3 @@ ax.set_xlabel('x')
 ax.set_ylabel('y')
 
 
-""" Plotting the Obstacle Potential """
-
-
-def obstacle_potential(x, y, obstacle=(0.3, 0.2), radius=0.3):
-    c = 6 * radius**3 / 2
-    potential = c / ((x - obstacle[0])** 2 + (y - obstacle[1])** 2)
-    # Capping value when within the obstacle just for
-    # plotting purposes
-    for i in range(np.shape(x)[0]):
-        for j in range(np.shape(x)[1]):
-            if (x[i, j] - obstacle[0])** 2 + (y[i, j] - obstacle[1])** 2 < radius**2:
-                potential[i, j] = c / radius** 2
-                pass
-    return potential
-
-x = np.linspace(-2, 2, grid_spacing)
-y = np.linspace(-2, 2, grid_spacing)
-
-X, Y = np.meshgrid(x, y)
-Z = obstacle_potential(X, Y)
-
-fig = plt.figure(2)
-ax = plt.axes(projection='3d')
-ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=colourmap, edgecolor=edgecol, linewidth=edgew)
-ax.set_zlabel('Potential')
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-
-
-""" Plotting the Combined Potential """
-
-
-x = np.linspace(-2, 2, grid_spacing)
-y = np.linspace(-2, 2, grid_spacing)
-
-X, Y = np.meshgrid(x, y)
-Z = obstacle_potential(X, Y) + goal_potential(X, Y)
-
-fig = plt.figure(3)
-ax = plt.axes(projection='3d')
-ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=colourmap, edgecolor=edgecol, linewidth=edgew)
-ax.set_zlabel('Potential')
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-
-
-""" Plotting the Issue in d) """
-
-
-x = np.linspace(-2, 2, grid_spacing)
-y = np.linspace(-2, 2, grid_spacing)
-
-X, Y = np.meshgrid(x, y)
-Z = obstacle_potential(X, Y, obstacle=(0, 0)) + goal_potential(X, Y)
-
-fig = plt.figure(4)
-ax = plt.axes(projection='3d')
-ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=colourmap, edgecolor=edgecol, linewidth=edgew)
-ax.set_zlabel('Potential')
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-
-
-""" Plotting the Mitigating solution in e) """
-
-
-x = np.linspace(-2, 2, grid_spacing)
-y = np.linspace(-2, 2, grid_spacing)
-
-X, Y = np.meshgrid(x, y)
-Z = obstacle_potential(X, Y, obstacle=(0, 0)) + goal_potential(X, Y) + obstacle_potential(X, Y, obstacle=(-1.5, -1.6), radius=0.1)
-
-fig = plt.figure(5)
-ax = plt.axes(projection='3d')
-ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=colourmap, edgecolor=edgecol, linewidth=edgew)
-ax.set_zlabel('Potential')
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-
-
-""" Plotting the Issue in f) """
-
-
-x = np.linspace(-1, 1, grid_spacing)
-y = np.linspace(-1, 1, grid_spacing)
-
-X, Y = np.meshgrid(x, y)
-Z = obstacle_potential(X, Y, obstacle=(0.5 ,0)) + obstacle_potential(X, Y, obstacle=(0, 0.5)) + goal_potential(X, Y)
-
-fig = plt.figure(6)
-ax = plt.axes(projection='3d')
-ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=colourmap, edgecolor=edgecol, linewidth=edgew)
-ax.set_zlabel('Potential')
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-
-
-""" Plotting the Mitigating solution to f) """
-
-
-x = np.linspace(-2, 2, grid_spacing)
-y = np.linspace(-2, 2, grid_spacing)
-
-X, Y = np.meshgrid(x, y)
-Z = obstacle_potential(X, Y, obstacle=(0.5 ,0)) + obstacle_potential(X, Y, obstacle=(0, 0.5)) + goal_potential(X, Y) \
-    + obstacle_potential(X, Y, obstacle=(-1.5, -1.6), radius=0.1)  + obstacle_potential(X, Y, obstacle=(0, 0), radius=0.5)
-
-fig = plt.figure(7)
-ax = plt.axes(projection='3d')
-ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=colourmap, edgecolor=edgecol, linewidth=edgew)
-ax.set_zlabel('Potential')
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-plt.show()
