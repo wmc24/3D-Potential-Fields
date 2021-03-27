1225c0d1e8b694abde13172eac7e0002f2541010
wmc24 <willcatton@gmail.com>
2021-03-27 18:51:16 +0000

    added basic data generator and training

--- nn_training.py
+++ nn_training.py
@@ -1,6 +1,16 @@
 import os
+import sys
 import torch
 import myModel
+import datetime
+import potential_field
+import numpy as np
+import torch.nn as nn
+from tqdm import tqdm as tqdm
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from torch.utils.data import Dataset as BaseDataset
+
 
 
 """-------------------------------------------------------------------------"""
@@ -14,11 +24,14 @@ DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 load_model = True
 loaded_model_name = 'Debugging'
 save_model_name = 'Debugging'
+tensorboard_name = save_model_name
 perform_training = True
 
 # Number of epochs to train for, if None then until keyboard interrupt (ctrl+c)
-num_epochs = 1 
+# and training parameters
+num_epochs = 10
 learning_rate = 1e-5
+batch_size = 10
 
 # How many epochs to save things
 model_epochs = 100
@@ -43,9 +56,8 @@ run_datestamp = f'{date} {hour}_{minute}_{second}'
 
 if perform_training is True:
     tensorboard_name = f'{run_datestamp} {tensorboard_name}'
-    writer = SummaryWriter(log_dir=os.path.join(EXPERIMENT_DIR, 'logs'))
+    writer = SummaryWriter(log_dir='logs')
 
-json_dir = 'models'
 save_dir = os.path.join('models', f'{save_model_name}.tar')
 save_dir_backup = os.path.join('models', f'{save_model_name}_backup.tar')
 load_dir = os.path.join('models', f'{loaded_model_name}.tar')
@@ -55,7 +67,7 @@ print("Device Used:", DEVICE)
 
 
 """-------------------------------------------------------------------------"""
-"""--------------------------Creating a Model-------------------------------"""
+"""--------------Creating a Model and setting up optimiser------------------"""
 """-------------------------------------------------------------------------"""
 
 
@@ -82,22 +94,95 @@ else:
 
 
 """-------------------------------------------------------------------------"""
+"""----------------------Generating data for training-----------------------"""
+"""-------------------------------------------------------------------------"""
+
+
+class Dataset(BaseDataset):
+    """
+    Randomly generating a scenario and returning the potential/velocity
+    for it. We return the following using relative coordinates:
+    
+    goal_position - numpy array (dims)
+    planet_positions - numpy array (N, dims)
+    planet_radii - numpy array (N, 1)
+    spaceships - numpy array (N, dims*2)
+    meteoroids - numpy array (N, dims*2)
+    """
+
+    def __init__(
+            self,
+            planets_pot=False,
+            spaceships_pot=False,
+            meteoroids_pot=False,
+            size_of_universe=10,
+            dims=2
+    ):
+        self.planets_pot = planets_pot
+        self.spaceships_pot = spaceships_pot
+        self.meteoroids_pot = meteoroids_pot
+        self.dims = 2
+
+    def __getitem__(self, i):
+        goal_position = [i, i, i]
+        planet_positions = []
+        planet_radii = []
+        spaceships = []
+        meteoroids = []
+        velocity = [1, 2, 3]
+
+        # Reshaping to the desired shapes and converting to numpy arrays
+        goal_position = np.array(goal_position)
+        planet_positions = np.array(planet_positions).reshape((-1, self.dims))
+        planet_radii = np.array(planet_radii).reshape((-1, 1))
+        spaceships = np.array(spaceships).reshape((-1, self.dims*2))
+        meteoroids = np.array(meteoroids).reshape((-1, self.dims*2))
+        velocity = np.array(velocity) #MIGHT REMOVE THIS
+
+
+
+        return goal_position, planet_positions, planet_radii, spaceships, meteoroids, velocity
+        
+    def __len__(self):
+        return batch_size
+
+
+train_dataset = Dataset()
+dataloader = DataLoader(train_dataset, batch_size=batch_size)
+
+
+"""-------------------------------------------------------------------------"""
 """-------------------------Training the Model------------------------------"""
 """-------------------------------------------------------------------------"""
 
 
 if perform_training is True:
     print("Training")
+    model.train()
+    loss = nn.MSELoss().to(DEVICE)
     try:
         i = epoch + 1
         while True:
-            print('\nEpoch: {}'.format(i))
-            # INSERT TRAINING
+            # Perfoming training
+            with tqdm(dataloader, desc='\nEpoch: {}'.format(i), file=sys.stdout) as iterator:
+                for goal_position, planet_positions, planet_radii, spaceships, meteoroids, velocity in iterator:
+                    goal_position = goal_position.to(DEVICE).float()
+                    planet_positions = planet_positions.to(DEVICE).float()
+                    planet_radii = planet_radii.to(DEVICE).float()
+                    spaceships = spaceships.to(DEVICE).float()
+                    meteoroids = meteoroids.to(DEVICE).float()
+                    velocity = velocity.to(DEVICE).float()
+
+                    optimizer.zero_grad()
+                    prediction = model.forward(goal_position, planet_positions, planet_radii, spaceships, meteoroids)
+                    total_loss = loss(prediction, velocity)
+                    total_loss.backward()
+                    optimizer.step()
 
             # Saving the model every n epochs and saving a backup in
             # case of a model corruption when interrupting
             if i % model_epochs == 0:
-                model_data = {'epoch': epoch,
+                model_data = {'epoch': i,
                               'model_state_dict': model.state_dict(),
                               'optimizer_state_dict': optimizer.state_dict()}
                 torch.save(model_data, save_dir)
@@ -107,7 +192,7 @@ if perform_training is True:
             # Saving a model checkpoint at a fixed number of epochs to go
             # back and test different versions
             if i % model_checkpoint_epochs == 0:
-                model_data = {'epoch': epoch,
+                model_data = {'epoch': i,
                               'model_state_dict': model.state_dict(),
                               'optimizer_state_dict': optimizer.state_dict()}
                 checkpoint_dir = os.path.join('models', f'{save_model_name}_{i}.tar')
@@ -117,7 +202,7 @@ if perform_training is True:
             # Checking whether or not to stop training
             if num_epochs is not None:
                 if i == num_epochs + epoch:
-                    model_data = {'epoch': epoch,
+                    model_data = {'epoch': i,
                                   'model_state_dict': model.state_dict(),
                                   'optimizer_state_dict': optimizer.state_dict()}
                     torch.save(model_data, save_dir)
@@ -125,7 +210,7 @@ if perform_training is True:
                     break
             i += 1
     except KeyboardInterrupt:
-        model_data = {'epoch': epoch,
+        model_data = {'epoch': i,
                       'model_state_dict': model.state_dict(),
                       'optimizer_state_dict': optimizer.state_dict()}
         torch.save(model_data, save_dir)
