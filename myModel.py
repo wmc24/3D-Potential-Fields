7cdbaf72637aaef3d46679c0f1680f9b390c3f9a
wmc24 <willcatton@gmail.com>
2021-03-27 18:50:26 +0000

    fixed more syntax errors

--- myModel.py
+++ myModel.py
@@ -11,16 +11,21 @@ class Net(nn.Module):
       self.linear1 = nn.Linear(3, 16)
       self.linear2 = nn.Linear(16, 3)
 
-    # x represents our data
-    def forward(self, x):
+    # The passed arguements are of the dimensions:
+    # goal_position - (dims)
+    # planet_positions - (N, dims)
+    # planet_radii - (N, 1)
+    # spaceships - (N, dims*2)
+    # meteoroids - (N, dims*2)
+    def forward(self, goal_position, planet_positions, planet_radii, spaceships, meteoroids):
       # Pass data through first linear layer
-      x = self.linear1(x)
+      x = self.linear1(goal_position)
       # Use leaky ReLU
       x = F.leaky_relu(x)
       # Pass data through second linear layer
-      x = self.linear1(x)
+      x = self.linear2(x)
       # Use tanh actiavtion function as we scale
-      # the output by the max speed
-      output = F.tanh(x)
+      # the output by the max speed CHANGE THIS
+      output = torch.tanh(x)
 
       return output
